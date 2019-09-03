084e1e3fb2f3ef79d6dc364327a205adb62a3c92
Marc-Antoine Loignon <developer@lognoz.org>
2019-09-03 00:40:35 -0400

    Core: Add theme loader into core-base.el

--- core/core-base.el
+++ core/core-base.el
@@ -18,7 +18,36 @@
 (defun core-base/init ()
   "Perform core base initialization."
   (core-base/create-elpa-repository)
-  (core-base/setup-ido))
+  (core-base/setup-ido)
+  (core-base/setup-theme))
+
+(defun core-base/setup-theme ()
+  "Define Emacs theme."
+  (install (atom-one-dark-theme))
+  ; To remove
+  (defvar atom-one-dark-colors-alist
+          `(("atom-one-dark-accent"   . "#528BFF")
+            ("atom-one-dark-fg"       . "#ABB2BF")
+            ("atom-one-dark-bg"       . "#000000")
+            ("atom-one-dark-bg-1"     . "#121417")
+            ("atom-one-dark-bg-hl"    . "#2C323C")
+            ("atom-one-dark-gutter"   . "#4B5363")
+            ("atom-one-dark-mono-1"   . "#ABB2BF")
+            ("atom-one-dark-mono-2"   . "#828997")
+            ("atom-one-dark-mono-3"   . "#5C6370")
+            ("atom-one-dark-cyan"     . "#56B6C2")
+            ("atom-one-dark-blue"     . "#61AFEF")
+            ("atom-one-dark-purple"   . "#C678DD")
+            ("atom-one-dark-green"    . "#98C379")
+            ("atom-one-dark-red-1"    . "#E06C75")
+            ("atom-one-dark-red-2"    . "#BE5046")
+            ("atom-one-dark-orange-1" . "#D19A66")
+            ("atom-one-dark-orange-2" . "#E5C07B")
+            ("atom-one-dark-gray"     . "#3E4451")
+            ("atom-one-dark-silver"   . "#9DA5B4")
+            ("atom-one-dark-black"    . "#000000")
+            ("atom-one-dark-border"   . "#000000")))
+  (load-theme 'atom-one-dark t))
 
 (defun core-base/setup-ido ()
   "Setup ido mode and use vertical aligment."
