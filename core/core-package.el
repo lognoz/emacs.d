5cadf7a2187bbfaa11bd90028b13ad90f470c926
Marc-Antoine Loignon <developer@lognoz.org>
2019-09-03 02:49:30 -0400

    Core: Add evil configurations to core-package.el

--- core/core-package.el
+++ core/core-package.el
@@ -7,6 +7,15 @@
 ;;
 ;; This file is not part of GNU Emacs.
 
+(defun stage-current-buffer ()
+  "Stage changes of active buffer."
+  (interactive)
+  (let* ((buffile (buffer-file-name))
+    (output (shell-command-to-string
+            (concat "git add " (buffer-file-name)))))
+    (message (if (not (string= output ""))
+        output (concat "Added " buffile)))))
+
 (defun core-package/setup-theme ()
   "Define Emacs theme."
   (add-package (atom-one-dark-theme))
@@ -24,5 +33,31 @@
         ido-create-new-buffer 'always
         ido-use-filename-at-point nil
         ido-max-prospects 10))
+
+(defun core-package/setup-evil ()
+  "Setup evil mode."
+  (add-package (evil
+                evil-collection
+                evil-indent-plus
+                evil-leader
+                evil-magit
+                evil-smartparens
+                evil-surround))
+  (global-evil-leader-mode 1)
+  (global-evil-surround-mode 1)
+  (evil-mode 1)
+  (core-package/setup-evil-leader))
+
+(defun core-package/setup-evil-leader ()
+  "Setup evil mode leader key."
+  (evil-leader/set-leader "<SPC>")
+  (evil-leader/set-key
+    "f"  'find-file
+    "t"  'dired
+    "w"  'save-buffer
+    "ga" 'stage-current-buffer
+    "gs" 'magit-status
+    "gc" 'magit-commit-create
+    "gl" 'magit-log-all))
 
 (provide 'core-package)
