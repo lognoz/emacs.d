7d0f98f2ba805083a3157a61afa2431ece7d0041
Marc-Antoine Loignon <developer@lognoz.org>
2019-09-09 16:26:47 -0400

    Init: Add component and language directories loader

--- init.el
+++ init.el
@@ -1,25 +1,44 @@
-;;; init.el - Initialization File
-;;
-;; Copyright (c) 2019-2019 Marc-Antoine Loignon & Contributors
-;;
-;; Author: Marc-Antoine Loignon <developer@lognoz.org>
-;; URL: https://github.com/lognoz/emacs.d
-;;
+;;; init.el --- Initialization File
+
+;; Copyright (c) 2019-2019 Marc-Antoine Loignon
+
+;; Author: Marc-Antoine Loignon <developer@lognoz>
+;; Keywords: init
+
 ;; This file is not part of GNU Emacs.
 
-(defun load-dir (dir) 
+;; This Emacs config is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the Free
+;; Software Foundation, either version 3 of the License, or (at your option)
+;; any later version.
+
+;; This Emacs config is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this Emacs config. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(defun load-dir (dir)
   "This function help to load directories."
   (add-to-list 'load-path dir))
 
 (defconst user-core-directory (concat user-emacs-directory "core/")
   "The root directory of core files.")
 
-(defconst user-module-directory (concat user-emacs-directory "module/")
-  "The root directory for module files.")
+(defconst user-component-directory (concat user-emacs-directory "component/")
+  "The root directory for component files.")
+
+(defconst user-language-directory (concat user-emacs-directory "language/")
+  "The root directory for language files.")
 
 ;; Load user directories.
 (load-dir user-core-directory)
-(load-dir user-module-directory)
+(load-dir user-component-directory)
+(load-dir user-language-directory)
 
 ;; Perform initialization.
 (require 'core)
