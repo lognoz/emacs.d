25080399db1ee8b5366d64a87dfb473d7d24c094
Marc-Antoine Loignon <developer@lognoz.org>
2019-09-22 03:09:04 -0400

    Evil: Move evil component file to evil directory

--- component/evil/configs.el
+++ component/evil/configs.el
@@ -1,4 +1,4 @@
-;;; component-evil.el - Evil Component File
+;;; configs.el - Evil Component File
 
 ;; Copyright (c) 2019-2019 Marc-Antoine Loignon
 
@@ -22,40 +22,30 @@
 
 ;;; Code:
 
-(defun component-evil/install ()
-  "Install Evil requirements."
-  (add-package (evil
-                evil-collection
-                evil-indent-plus
-                evil-leader
-                evil-magit
-                evil-smartparens
-                evil-surround)))
-
-(defun component-evil/setup-evil ()
-  "Configure evil mode."
-  (require 'evil-magit)
-  (global-evil-leader-mode 1)
-  (global-evil-surround-mode 1)
+(defun evil-initialize ()
+  (require-package (evil
+                    evil-collection
+                    evil-indent-plus
+                    evil-leader
+                    evil-magit
+                    evil-smartparens
+                    evil-surround))
+  (evil--setup-leader)
+  (evil-surround-mode 1)
   (evil-mode 1))
 
-(defun component-evil/setup-leader ()
-  "Configure evil leader mode."
+(defun evil--setup-leader ()
+  (global-evil-leader-mode 1)
   (evil-leader/set-leader "<SPC>")
   (evil-leader/set-key
     "f"   'find-file
     "d"   'kill-this-buffer
     "w"   'save-buffer
-    "t"   'open-current-directory
     "ga"  'stage-current-buffer
     "gs"  'magit-status
     "gc"  'magit-commit-create
     "gl"  'magit-log-all
     "gps" 'magit-push-current-to-upstream)
-
-  (defun open-current-directory ()
-    (interactive)
-    (dired (expand-file-name ".")))
 
   (defun stage-current-buffer ()
     "Stage changes of active buffer."
@@ -65,9 +55,3 @@
               (concat "git add " (buffer-file-name)))))
       (message (if (not (string= output ""))
           output (concat "Added " buffile))))))
-
-(component-evil/install)
-(component-evil/setup-evil)
-(component-evil/setup-leader)
-
-(provide 'component-evil)
