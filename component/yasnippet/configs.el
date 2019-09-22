2f65b9cf24633aa2866fd937e3494d54eba8a640
Marc-Antoine Loignon <developer@lognoz.org>
2019-09-22 03:50:13 -0400

    Yasnippet: Add yasnippet as snippet manager

---
+++ component/yasnippet/configs.el
@@ -0,0 +1,29 @@
+;;; configs.el - Yasnippet Component File
+
+;; Copyright (c) 2019-2019 Marc-Antoine Loignon
+
+;; Author: Marc-Antoine Loignon <developer@lognoz.org>
+;; Keywords: snippet
+
+;; This file is not part of GNU Emacs.
+
+;; This Emacs config is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the Free
+;; Software Foundation, either version 3 of the License, or (at your option)
+;; any later version.
+
+;; This Emacs config is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this Emacs config. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(require-package (yasnippet ))
+
+(defun yasnippet-initialize ()
+  (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
+  (yas-global-mode 1))
