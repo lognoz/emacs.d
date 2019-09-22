4b6d835c0a7ae15205d725b7a37d62896a4e3869
Marc-Antoine Loignon <developer@lognoz.org>
2019-09-22 03:04:59 -0400

    Core: Add function to sudo the current buffer easily

---
+++ core/core-funcs.el
@@ -0,0 +1,31 @@
+;;; core-funcs.el - Core Functions File
+
+;; Copyright (c) 2019-2019 Marc-Antoine Loignon
+
+;; Author: Marc-Antoine Loignon <developer@lognoz.org>
+;; Keywords: functions helpers utils
+
+;; This file is not part of GNU Emacs.
+
+;; This Emacs config is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the Free
+;; Software Foundation, either version 3 of the License, or (at your option)
+;; any later version.
+
+;; This Emacs config is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this Emacs config. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(defun sudo ()
+  "Use TRAMP to `sudo' the current buffer"
+  (interactive)
+  (when buffer-file-name
+    (find-alternate-file
+     (concat "/sudo:root@localhost:"
+             buffer-file-name))))
