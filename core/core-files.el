58c6e3b14b5c8d1f958e0f3fc74770f925dafe20
Marc-Antoine Loignon <developer@lognoz.org>
2019-10-02 23:39:38 -0400

    Core: Add function to backup files on save

--- core/core-files.el
+++ core/core-files.el
@@ -22,13 +22,52 @@
 
 ;;; Code:
 
+(defun core-files//save-backup ()
+  ;; Make a special "per session" backup at the first save of each
+  ;; emacs session.
+  (when (not buffer-backed-up)
+    ;; Override the default parameters for per-session backups.
+    (let ((backup-directory-alist '(("" . "~/.emacs.d/backup/per-session")))
+          (kept-new-versions 3))
+      (backup-buffer)))
+  ;; Make a "per save" backup on each save.  The first save results in
+  ;; both a per-session and a per-save backup, to keep the numbering
+  ;; of per-save backups consistent.
+  (let ((buffer-backed-up nil))
+    (backup-buffer)))
+
+(defun core-files//setup-backup ()
+  (setq version-control t     ; Use version numbers for backups.
+        kept-new-versions 8   ; Number of newest versions to keep.
+        kept-old-versions 0   ; Number of oldest versions to keep.
+        delete-old-versions t ; Don't ask to delete excess backup versions.
+        backup-by-copying t)  ; Copy all files, don't rename them.
+
+  (setq vc-make-backup-files t)
+
+  ;; Default and per-save backups go here
+  (setq backup-directory-alist '(("" . "~/.emacs.d/backup/per-save")))
+
+  (add-hook 'before-save-hook 'core-files//save-backup))
+
 (defun core-files/embla-startup-hook ()
+  ;; Save every 20 characters typed
+  (setq auto-save-interval 20)
+
   ;; Backup file
-  (setq backup-directory-alist
-        (list (cons "." (expand-file-name "backup" embla-temporary-directory))))
+  (core-files//setup-backup)
 
   ;; Bookmark file
   (setq bookmark-default-file (concat embla-temporary-directory "bookmark"))
+
+  ; Save minibuffer
+  (savehist-mode 1)
+  (setq savehist-file (concat embla-temporary-directory "savehist"))
+  (setq history-length 100)
+
+  ; Save cursor positions
+  (save-place-mode 1)
+  (setq save-place-file (concat embla-temporary-directory "saveplace"))
 
   ;; Undo file
   (packadd! undo-tree)
