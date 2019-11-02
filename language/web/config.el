3d33eacae662755ca58c4fcf8b4e8e3f9c5c2a08
Marc-Antoine Loignon <developer@lognoz.org>
2019-11-02 16:01:12 -0400

    Web: Add initial web mode configuration

---
+++ language/web/config.el
@@ -0,0 +1,34 @@
+;;; config.el - Web Language File
+
+;; Copyright (c) 2019-2019 Marc-Antoine Loignon
+
+;; Author: Marc-Antoine Loignon <developer@lognoz.org>
+;; Keywords: web html css
+
+;; This file is not part of GNU Emacs.
+
+;; This Emacs config is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the Free
+;; Software Foundation, either version 3 of the License, or (at your option)
+;; any later version.
+
+;; This Emacs config is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this Emacs config. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(defun web/hook-web-mode ()
+  (require 'company)
+  (require 'company-web-html)
+  (require 'emmet-mode)
+
+  (set (make-local-variable 'company-backends)
+       '(company-css company-web-html company-yasnippet company-files))
+
+  (company-mode t)
+  (emmet-mode))
