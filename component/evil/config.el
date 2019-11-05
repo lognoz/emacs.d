21f72565702e8cee672a7d9781e3ce3d6b99d6d1
Marc-Antoine Loignon <developer@lognoz.org>
2019-11-05 00:54:46 -0500

    Evil: Add package for better indent text objects

--- component/evil/config.el
+++ component/evil/config.el
@@ -71,6 +71,14 @@
   (with-eval-after-load 'magit
     (require 'evil-magit)))
 
+(defun evil/init-evil-indent-plus ()
+  (define-key evil-inner-text-objects-map "i" 'evil-indent-plus-i-indent)
+  (define-key evil-outer-text-objects-map "i" 'evil-indent-plus-a-indent)
+  (define-key evil-inner-text-objects-map "I" 'evil-indent-plus-i-indent-up)
+  (define-key evil-outer-text-objects-map "I" 'evil-indent-plus-a-indent-up)
+  (define-key evil-inner-text-objects-map "J" 'evil-indent-plus-i-indent-up-down)
+  (define-key evil-outer-text-objects-map "J" 'evil-indent-plus-a-indent-up-down))
+
 (defun evil/init-evil-leader ()
   (evil-leader/set-leader "<SPC>")
   (evil-leader/set-key
