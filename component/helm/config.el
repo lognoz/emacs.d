19e54bdcb892c8546d122d84969fdaea1eb9b3ab
Marc-Antoine Loignon <developer@lognoz.org>
2019-11-08 02:42:50 -0500

    Helm: Add keybinding for imenu

--- component/helm/config.el
+++ component/helm/config.el
@@ -85,6 +85,7 @@
 (defun helm//setup-keybindings ()
   (global-set-key (kbd "M-x") 'helm-M-x)
   (global-set-key (kbd "C-x b") 'helm-buffers-list)
+  (global-set-key (kbd "C-c C-j") 'helm-imenu)
   (global-set-key (kbd "C-x r b") 'helm-filtered-bookmarks)
   (global-set-key (kbd "C-x C-f") 'helm-find-files)
   (global-set-key (kbd "M-y") 'helm-show-kill-ring)
