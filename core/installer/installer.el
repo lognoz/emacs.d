4ed48d7b18b9046cfc741e55c19f1846d113b795
Marc-Antoine Loignon <developer@lognoz.org>
2020-03-31 01:23:39 -0400

    Core: Optimize installer file and rename templates

--- core/installer/installer.el
+++ core/installer/installer.el
@@ -24,196 +24,188 @@
 
 ;;; Contextual core variables.
 
-(defvar template-component-hook-function
-  (template-content
-    (concat embla-core-directory "installer/template/component-hook-function")))
+(defconst installer-template-directory (concat embla-core-directory "installer/template/")
+  "The directory of template files.")
 
-(defvar template-component-hook-statement
-  (template-content
-    (concat embla-core-directory "installer/template/component-hook-statement")))
+(defconst template-auto-mode-alist
+  (template-content (concat installer-template-directory "auto-mode-alist")))
 
-(defvar template-simple-hook-statement
-  (template-content
-    (concat embla-core-directory "installer/template/simple-hook-statement")))
+(defconst template-auto-install-hook
+  (template-content (concat installer-template-directory "auto-install-hook")))
 
-(defvar component-autoload-file-content nil
-  "The content of autoload component file.")
+(defconst template-hook-function
+  (template-content (concat installer-template-directory "hook-function")))
 
-(defvar component-content nil
-  "The content of component file.")
+(defconst template-hook-statement
+  (template-content (concat installer-template-directory "hook-statement")))
 
-;;; Internal core functions.
+(defconst template-simple-hook-statement
+  (template-content (concat installer-template-directory "simple-hook-statement")))
 
-(defun directory-name (path)
-  "This function is used to get directory name."
-  (file-name-nondirectory
-   (directory-file-name
-     (file-name-directory path))))
+(defconst auto-install-components-alist
+  ;; Extension              Word syntax   Require   Mode
+  '(("\\.js\\'"             '("-" "_")    nil       js2-mode)
+    ("\\.scss\\'"           '("-" "_")    nil       scss-mode)
+    ("\\.pp\\'"             '("-" "_")    nil       puppet-mode)
+    ("PKGBUILD\\'"          '("-" "_")    nil       pkgbuild-mode)
+    ("Dockerfile\\'"        '("-" "_")    nil       dockerfile-mode)
+    ("\\.yml\\'"            '("-" "_")    nil       yaml-mode)
+    ("\\.json\\'"           '("-" "_")    nil       json-mode)
+    ("\\.tex\\'"            '("\\")       t         latex-mode)
+    ("\\.editorconfig\\'"   '("-" "_")    t         editorconfig-conf-mode)))
 
-(defun directories (path)
-  "This function return a list of directories by a reference path."
-  (let ((directories))
-    (dolist (f (directory-files path))
-      (let ((path (concat path f)))
-        (when (and (file-directory-p path)
-                    (not (equal f "."))
-                    (not (equal f "..")))
-          (push (file-name-as-directory path) directories))))
-    directories))
+;;; Internal installer functions.
 
-(defun delete-documentation-header ()
-  "This function is used to delete documentation header"
-  (let ((st (line-beginning-position)))
-    (search-forward ";;; Code:")
-    (delete-region st (point)))
-  (delete-blank-lines))
+(defmacro load-files (path files &rest body)
+  "Load multiples files by reference path."
+  `(dolist (f ,files)
+    (when (file-exists-p (concat ,path f ".el"))
+      (load (concat ,path f) nil 'nomessage)
+      ,@body)))
 
-(defun create-component-file ()
-  "This function scan component directory to extract all autoload
-files and merge it into one file."
-  ;; Add the provide statement as last line of the content.
-  (push "(provide 'embla-component)" component-autoload-file-content)
-  ;; Extract all autoload.el in component directories to push it to
-  ;; `component-autoload-file-content' variable.
-  (dolist (path (directory-files-recursively (concat embla-component-directory "/startup/") ""))
-    (when (string-equal (file-name-base path) "autoload")
-      (push (with-temp-buffer
-              (insert-file-contents path)
-              (delete-documentation-header)
-              (buffer-string))
-        component-autoload-file-content)))
-  ;; Add extracted content to file.
+(defun file-content-without-header (path)
+  "Return file content witout documentation header."
+  (with-temp-buffer
+    (insert-file-contents path)
+    (let ((st (line-beginning-position)))
+      (search-forward ";;; Code:")
+      (delete-region st (point)))
+    (delete-blank-lines)
+    (buffer-string)))
+
+(defun append-to-file (path content)
+  "Write content into the end of a file."
   (write-region
-    (mapconcat #'identity component-autoload-file-content "\n")
-    nil embla-component-file))
+    (mapconcat #'identity content "\n") nil path))
 
-(defun create-autoload-file (&optional force)
-  "This function parse magic comments locate in core and project
-directories and append it to autoload file locate. It will helps to
-optimize Embla."
-  (let ((generated-autoload-file embla-autoload-file))
-    ;; Clear content in autoload file.
-    (with-current-buffer (find-file-noselect generated-autoload-file)
-      (insert "")
-      (save-buffer))
-    ;; Update autoload with embla component file generated earlier.
-    (dolist (path (directory-files-recursively embla-build-directory ""))
-      (update-file-autoloads path t generated-autoload-file))
-    ;; Update autoload with core directory.
-    (dolist (path (directory-files-recursively embla-core-directory ""))
-      (update-file-autoloads path t generated-autoload-file))
-    ;; Update autoload with project directory.
-    (dolist (path (directory-files-recursively embla-project-directory ""))
-      (update-file-autoloads path t generated-autoload-file))))
+(defun auto-install-content (package)
+  "Return content of package hooks and auto-mode-alist. It's
+mainly used with mapconcat."
+  (let ((extension (prin1-to-string (car package)))
+        (syntax (prin1-to-string (cadr package)))
+        (built-in (cadr (cdr package)))
+        (mode (cadr (cdr (cdr package)))))
+    (unless built-in
+      (require-package mode))
+    (setq mode (symbol-name mode))
+    (concat
+      (replace-in-string template-auto-install-hook
+        '((cons "__mode__" mode)
+          (cons "__syntax__" syntax)))
+      (replace-in-string template-auto-mode-alist
+        '((cons "__mode__" mode)
+          (cons "__extension__" extension))))))
+
+(defun create-auto-install-file ()
+  "Create a file that contains all auto-installed packages."
+  (let ((path (concat embla-build-directory "component-auto-install.el")))
+    (write-region
+      (mapconcat #'auto-install-content auto-install-components-alist "\n")
+      nil path)))
 
 (defun refresh-package-repositories ()
-  "This function is used to set and refresh package repositories."
+  "Set archives and refresh package repositories."
   (message "Download descriptions of configured ELPA packages")
   (setq embla-package-initialized t)
   (package-set-archives)
   (package-refresh-contents))
 
-(defun build-component-files (directory component)
+(defun create-startup-autoload-file ()
+  "Scan component directory to extract all autoload files and
+merge it into one file."
+  (let ((path-reference (concat embla-component-directory "/startup/"))
+        (file-content '("(provide 'embla-component)")))
+    ;; Extract all autoload.el in component directories to push it to
+    ;; `component-autoload-file-content' variable.
+    (dolist (path (directory-files-recursively path-reference ""))
+      (when (string-equal (file-name-base path) "autoload")
+        (push (file-content-without-header path)
+              file-content)))
+    (append-to-file embla-component-file file-content)))
+
+(defun create-startup-component-file ()
+  "Extract all content into subdirectories locate in startup
+component to write an unique file that contains it."
+  (let ((file-content '("(provide 'embla-startup)")))
+    (fetch-content (concat embla-component-directory "startup/")
+      ;; Put this variable to nil to know what package is install inside
+      ;; this component.
+      (setq embla-component-packages nil)
+      ;; Loop into packages installed in component to create init
+      ;; function caller.
+      (load-files path '("package" "config"))
+      (dolist (dependency embla-component-packages)
+        (when-function-exists (concat module "-init-" dependency)
+          (push (format "(%s)" func) file-content)))
+      (push (file-content-without-header (concat path "/config.el"))
+            file-content)
+      (append-to-file embla-startup-file file-content))))
+
+(defun create-autoload-file ()
+  "This function parse magic comments locate in core and project
+directories and append it to autoload file locate. It will helps to
+optimize Embla."
+  (let ((generated-autoload-file embla-autoload-file)
+        (directories '(embla-build-directory embla-core-directory embla-project-directory)))
+    ;; Clear content in autoload file.
+    (with-current-buffer (find-file-noselect generated-autoload-file)
+      (insert "")
+      (save-buffer))
+    ;; Update autoload on build, core and project directories.
+    (dolist (directory directories)
+      (dolist (path (directory-files-recursively (symbol-value directory) ""))
+        (unless (equal (directory-name path) "template")
+          (update-file-autoloads path t generated-autoload-file))))))
+
+(defun make-component-files (directory component)
+  "Fetch directories to create component file."
   (fetch-content directory
     (let ((mode (concat module "-" component "-mode")))
-      (build-component-file
+      (make-component-file
         path module component mode))))
 
-(defun file-contents (path)
-  (with-temp-buffer
-    (insert-file-contents path)
-    (delete-documentation-header)
-    (buffer-string)))
-
-(defun build-component-file (path module component mode)
-  ;; Remove file content because it cause problem when user rebuild
-  ;; it on the same session.
-  (setq component-content nil)
-  ;; Put this variable to nil to know what package is install inside
-  ;; this component.
-  (setq embla-component-packages nil)
-  ;; Add the provide statement as last line of the content.
-  (push (format "(provide 'embla-%s-%s-component)" module component)
-        component-content)
-  ;; Add template hook function.
-  (push template-component-hook-function component-content)
-  ;; Load component files.
-  (dolist (f '("package" "autoload" "config"))
-    (when (file-exists-p (concat path f ".el"))
-      (load (concat path f) nil 'nomessage)
+(defun make-component-file (path module component mode)
+  "Create component file by given subdirectories arguments."
+  (let* ((provider (format "(provide 'component-%s-%s)" component module))
+         (file-to-write
+           (format "%scomponent-%s-%s.el"
+                   embla-build-directory component module))
+         (file-content (list template-hook-function provider))
+         (variable-init-content))
+    ;; Install packages and append content of autoload and config.
+    (load-files path '("package" "autoload" "config")
       (unless (equal f "package")
-        (push (file-contents (concat path f ".el")) component-content))))
-  ;; Build hooks for the module.
-  (push (build-hooks-in-module module component)
-        component-content)
-  ;; Loop into packages installed to add hooks.
-  (dolist (dependency embla-component-packages)
-    (when-function-exists (concat module "-hook-" dependency)
-      (push (format template-simple-hook-statement (concat dependency "-hook") func)
-            component-content)))
-  ;; Define path by variables.
-  (setq path
-    (format "%sembla-%s-%s-component.el"
-      embla-build-directory module component))
-  ;; Add extracted content to file.
-  (write-region
-    (mapconcat #'identity component-content "\n")
-    nil path)
-  ;; Loop into packages installed in component to create init
-  ;; function caller.
-  (setq component-content nil)
-  (dolist (dependency embla-component-packages)
-    (when-function-exists (concat module "-init-" dependency)
-      (push (format "(%s)" func) component-content)))
-  ;; Replace variables.
-  (replace-in-file path
-    '((cons "__module__" module)
-      (cons "__component__" component)
-      (cons "__content__" (mapconcat #'identity component-content "\n")))))
+        (push (file-content-without-header (concat path f ".el"))
+              file-content)))
+    ;; Build hooks for the module.
+    (push (hook-in-module module component)
+          file-content)
+    ;; Loop into packages installed to add hooks and create init
+    ;; function caller.
+    (dolist (dependency embla-component-packages)
+      (when-function-exists (concat module "-init-" dependency)
+        (push (format "(%s)" func) variable-init-content))
+      (when-function-exists (concat module "-hook-" dependency)
+        (push (format template-simple-hook-statement (concat dependency "-hook") func)
+              file-content)))
+    (append-to-file file-to-write file-content)
+    (replace-in-file file-to-write
+      '((cons "__module__" module)
+        (cons "__component__" component)
+        (cons "__content__" (mapconcat #'identity variable-init-content "\n"))))))
 
-(defun build-hooks-in-module (module component)
+(defun hook-in-module (module component)
+  "Return autoload hook statements by variable."
   (let* ((variable (concat module "-" component "-loader-hooks"))
          (reference (intern variable))
          (content))
     (when (boundp reference)
       (dolist (mode (symbol-value reference))
-        (push
-          (format template-component-hook-statement (symbol-name mode))
-          content))
+        (push (format template-hook-statement (symbol-name mode))
+              content))
       (mapconcat #'identity content "\n"))))
 
-(defun build-embla-startup-file ()
-  ;; Remove file content because it cause problem when user rebuild
-  ;; it on the same session.
-  (setq component-content nil)
-  ;; Add the provide statement as last line of the content.
-  (push "(provide 'embla-startup)" component-content)
-  ;; Fetch the startup directory locate in `embla-component-directory'.
-  (fetch-content (concat embla-component-directory "startup/")
-    ;; Put this variable to nil to know what package is install inside
-    ;; this component.
-    (setq embla-component-packages nil)
-    ;; Load component files.
-    (dolist (f '("package" "config"))
-      (when (file-exists-p (concat path f ".el"))
-        (load (concat path f) nil 'nomessage)))
-    ;; Loop into packages installed in component to create init
-    ;; function caller.
-    (dolist (dependency embla-component-packages)
-      (when-function-exists (concat module "-init-" dependency)
-        (push (format "(%s)" func) component-content)))
-    ;; Build content file and remove documentation header.
-    (push (with-temp-buffer
-            (insert-file-contents (concat path "/config.el"))
-            (delete-documentation-header)
-            (buffer-string))
-      component-content)
-    ;; Add extracted content to file.
-    (write-region
-      (mapconcat #'identity component-content "\n")
-      nil embla-startup-file)))
-
-;;; External core functions.
+;;; External installer functions.
 
 (defun embla-install-program ()
   "This function is the main installer function. It create autoload
@@ -221,20 +213,22 @@ file, refresh package repositories and build Embla."
   ;; Recreate build directory.
   (delete-directory embla-build-directory t)
   (make-directory embla-build-directory)
-  ;; Create component file that contains the autoloads functions in
-  ;; component directory.
-  (create-component-file)
   ;; Refresh package repositories.
   (refresh-package-repositories)
   ;; Install Embla theme.
   (require-package 'atom-one-dark-theme)
-  ;; Build file that's load on Emacs startup.
-  (build-embla-startup-file)
-  ;; Build component files.
+  ;; Create component file with `auto-install-alist'.
+  (create-auto-install-file)
+  ;; Create component file that contains the autoloads functions in
+  ;; component directory.
+  (create-startup-autoload-file)
+  ;; Create file that's load on Emacs startup.
+  (create-startup-component-file)
+  ;; Create component files.
   (dolist (directory (directories embla-component-directory))
     (let ((component (directory-name directory)))
       (unless (equal component "startup")
-        (build-component-files directory component))))
+        (make-component-files directory component))))
   ;; Scan core, component and project directories to create the
   ;; autoload file.
   (create-autoload-file))
