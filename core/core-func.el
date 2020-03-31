eb09bcfacda2e760d056e82a2fcf7c40a402219e
Marc-Antoine Loignon <developer@lognoz.org>
2020-03-31 01:34:13 -0400

    Core: Optimize functions into core-func file

--- core/core-func.el
+++ core/core-func.el
@@ -57,10 +57,6 @@
     replacement)
   string)
 
-;; (defun replace-in-string (regexp rep string)
-;;   ;;(mapc (lambda (entry)
-;;   (replace-regexp-in-string regexp rep string nil 'literal))
-
 (defun recursive-directories (path)
   "Return all directories in a specific path."
   (split-string
@@ -98,124 +94,75 @@
         (kill-buffer buffer)
         (message "File '%s' successfully removed" filename)))))
 
+(defun open-terminal ()
+  "Open terminal easily."
+  (interactive)
+  (let ((buffers (cdr (buffer-list))))
+    (while buffers
+      (when (with-current-buffer (car buffers) (string= "term-mode" major-mode))
+        (switch-to-buffer (car buffers))
+        (setq buffers nil))
+      (setq buffers (cdr buffers)))
+    (when (not (string= "term-mode" major-mode))
+      (ansi-term "/bin/bash")
+      (rename-buffer "Terminal"))))
 
+(defun open-terminal-other-window ()
+  "Open terminal in other window."
+  (interactive)
+  (when (one-window-p)
+    (split-window-right))
+  (other-window 1)
+  (open-terminal))
 
+;;; Eval and replace sexp.
 
+(defun verify-statement (message)
+  "Throw error if the cursor is not in a statement."
+  (when (and (not (or (> (nth 0 (syntax-ppss)) 0)
+                      (nth 3 (syntax-ppss))))
+             (not (equal (char-after) ?\()))
+    (error message)))
 
+(defun sexp-insert-mode ()
+  (if (or (equal evil-state 'normal)
+          (equal evil-state 'insert))
+    (evil-insert-state)
+    (error "This function can only be executed in normal or insert mode.")))
 
+(defun eval-and-replace ()
+  "Replace the preceding sexp with its value."
+  (interactive)
+  (sexp-insert-mode)
+  (verify-statement "This function only works on lisp sexp.")
+  (when (not (equal (char-after) ?\())
+    (backward-up-list))
+  (forward-sexp)
+  (backward-kill-sexp)
+  (condition-case nil
+    (prin1 (eval (read (current-kill 0)))
+          (current-buffer))
+    (error (message "Invalid expression")
+          (insert (current-kill 0)))))
 
+;;; Find file on cursor.
 
+(defun content-inner-quote (char)
+  (let* ((right (save-excursion
+                  (re-search-forward char nil t)))
+         (left (save-excursion
+                  (re-search-backward char nil t))))
+    (when (and right right)
+      (substring (buffer-substring-no-properties right left) 1 -1))))
 
-
-;; (defun byte-recompile (path)
-;;   "Byte compile a directory path."
-;;   (if (fboundp 'async-byte-recompile-directory)
-;;       (async-byte-recompile-directory path)
-;;     (byte-recompile-directory path)))
-
-;; (defun recompile-elpa ()
-;;   "Recompile packages in elpa directory."
-;;   (interactive)
-;;   (byte-recompile package-user-dir))
-
-;; (defun recompile-embla ()
-;;   "Recompile component and core directories."
-;;   (interactive)
-;;   (byte-recompile embla-core-directory)
-;;   (byte-recompile embla-component-directory))
-
-
-;; (defun open-terminal ()
-;;   "Open terminal easily."
-;;   (interactive)
-;;   (let ((buffers (cdr (buffer-list))))
-;;     (while buffers
-;;       (when (with-current-buffer (car buffers) (string= "term-mode" major-mode))
-;;         (switch-to-buffer (car buffers))
-;;         (setq buffers nil))
-;;       (setq buffers (cdr buffers)))
-;;     (when (not (string= "term-mode" major-mode))
-;;       (ansi-term "/bin/bash")
-;;       (rename-buffer "Terminal"))))
-
-;; (defun open-terminal-other-window ()
-;;   "Open terminal in other window."
-;;   (interactive)
-;;   (when (one-window-p)
-;;     (split-window-right))
-;;   (other-window 1)
-;;   (open-terminal))
-
-;; ;;; TODO: Kill buffer on same directory.
-
-;; (defun kill-buffers-by-directory-action (path)
-;;   (interactive
-;;     (list (read-directory-name
-;;             "Kill buffers by directory: "
-;;             (file-name-directory buffer-file-name))))
-;;   (let ((buffers (cdr (buffer-list)))
-;;         (buffers-to-kill))
-;;     (while buffers
-;;       (let ((file-name (buffer-file-name (car buffers))))
-;;         (when file-name
-;;           (prin1 (car buffers))))
-;;           ;;(prin1 (string-match path (file-name-directory file-name)))))
-;;       (setq buffers (cdr buffers)))))
-
-;; (defun kill-buffers-by-directory ()
-;;   (interactive)
-;;   (if (buffer-file-name)
-;;     (call-interactively 'kill-buffers-by-directory-action)
-;;     (error "Buffer '%s' is not visiting a file!" (buffer-name))))
-
-;; ;;; Eval and replace sexp.
-
-;; (defun verify-statement (message)
-;;   "Throw error if the cursor is not in a statement."
-;;   (when (and (not (or (> (nth 0 (syntax-ppss)) 0)
-;;                       (nth 3 (syntax-ppss))))
-;;              (not (equal (char-after) ?\()))
-;;     (error message)))
-
-;; (defun sexp-insert-mode ()
-;;   (if (or (equal evil-state 'normal)
-;;           (equal evil-state 'insert))
-;;     (evil-insert-state)
-;;     (error "This function can only be executed in normal or insert mode.")))
-
-;; (defun eval-and-replace ()
-;;   "Replace the preceding sexp with its value."
-;;   (interactive)
-;;   (sexp-insert-mode)
-;;   (verify-statement "This function only works on lisp sexp.")
-;;   (when (not (equal (char-after) ?\())
-;;     (backward-up-list))
-;;   (forward-sexp)
-;;   (backward-kill-sexp)
-;;   (condition-case nil
-;;     (prin1 (eval (read (current-kill 0)))
-;;           (current-buffer))
-;;     (error (message "Invalid expression")
-;;           (insert (current-kill 0)))))
-
-;; ;;; Find file on cursor.
-
-;; (defun content-inner-quote (char)
-;;   (let* ((right (save-excursion
-;;                   (re-search-forward char nil t)))
-;;          (left (save-excursion
-;;                   (re-search-backward char nil t))))
-;;     (when (and right right)
-;;       (substring (buffer-substring-no-properties right left) 1 -1))))
-
-;; (defun find-file-on-cursor ()
-;;   (interactive)
-;;   (let ((root (projectile-project-root))
-;;         (content (content-inner-quote "\"")))
-;;     (unless (file-exists-p (concat root content))
-;;       (setq content (content-inner-quote "'")))
-;;     (if (file-exists-p (concat root content))
-;;       (find-file (concat root content))
-;;       (error "Could not find file on point."))))
+(defun find-file-on-cursor ()
+  (interactive)
+  (let ((root (projectile-project-root))
+        (content (content-inner-quote "\"")))
+    (unless (file-exists-p (concat root content))
+      (setq content (content-inner-quote "'")))
+    (if (file-exists-p (concat root content))
+      (find-file (concat root content))
+      (error "Could not find file on point."))))
 
 (provide 'core-func)
