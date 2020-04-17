bfdf1748db179eb33dd477f6b23cfc412655c395
Marc-Antoine Loignon <developer@lognoz.org>
2020-04-17 00:22:14 -0400

    Component: Add keybindings to help component

---
+++ component/emacs/help/variable.el
@@ -0,0 +1,34 @@
+;;; variable.el --- Help Component Variable File
+
+;; Copyright (c) Marc-Antoine Loignon
+
+;; Author: Marc-Antoine Loignon <developer@lognoz.org>
+;; Keywords: help
+
+;; This file is not part of GNU Emacs.
+
+;; This Emacs config is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the Free
+;; Software Foundation, either version 3 of the License, or (at your option)
+;; any later version.
+
+;; This Emacs config is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this Emacs config. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(defvar help-emacs-loader-hooks '(help-mode-hook)
+  "The hook that load help emacs module.")
+
+(defvar help-emacs-keybindings
+  (define-keybinding
+    :mode 'help-mode-map
+    :normal
+      "<" 'help-go-back
+      ">" 'help-go-forward
+      "q" 'quit-window))
