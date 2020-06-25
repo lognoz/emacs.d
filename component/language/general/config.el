e11d99ad1ce5a861a62451395941d70844c436e2
Marc-Antoine Loignon <developer@lognoz.org>
2020-06-25 15:49:46 -0400

    Component: Add prog and text languages configuration

---
+++ component/language/general/config.el
@@ -0,0 +1,28 @@
+;;; config.el --- General Component Config File
+
+;; Copyright (c) Marc-Antoine Loignon
+
+;; Author: Marc-Antoine Loignon <developer@lognoz.org>
+;; Keywords: prog text
+
+;; This file is not part of GNU Emacs.
+
+;; This Emacs config is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the Free
+;; Software Foundation, either version 3 of the License, or (at your option)
+;; any later version.
+
+;; This Emacs config is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this Emacs config. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(defun general-init-linum-relative ()
+  (setq linum-relative-backend 'display-line-numbers-mode)
+  (display-line-numbers-mode)
+  (linum-relative-mode))
