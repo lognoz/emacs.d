f67873f620fead465a5293874a1c72358dbf5b07
Marc-Antoine Loignon <developer@lognoz.org>
2020-10-26 12:58:49 -0400

    Lisp: Add initial tools directory

---
+++ lisp/tools/pdf.el
@@ -0,0 +1,32 @@
+;;; lisp/tools/pdf.el --- pdf configurations -*- lexical-binding: t; -*-
+
+;; Copyright (c) Marc-Antoine Loignon
+
+;; Author: Marc-Antoine Loignon <developer@lognoz.org>
+;; Homepage: https://github.com/lognoz/embla
+;; Keywords: pdf
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Code:
+
+;;;###autoload
+(boot-packages 'pdf-tools)
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.pdf\\'" . pdf-tools-install))
+
+;;; pdf.el ends here
