cfaf9a42ea1bf3157fd72bf3a20533d9765cce96
Marc-Antoine Loignon <developer@lognoz.org>
2020-10-27 18:10:54 -0400

    Lisp: Add initial tramp file to autoloads directory

---
+++ lisp/autoloads/tramp.el
@@ -0,0 +1,35 @@
+;;; lisp/autoloads/tramp.el --- tramp autoloads -*- lexical-binding: t; -*-
+
+;; Copyright (c) Marc-Antoine Loignon
+
+;; Author: Marc-Antoine Loignon <developer@lognoz.org>
+;; Homepage: https://github.com/lognoz/embla
+;; Keywords: tramp
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Code:
+
+;;;###autoload
+(defun sudo ()
+  "Use TRAMP to `sudo` the current buffer"
+  (interactive)
+  (when buffer-file-name
+    (find-alternate-file
+     (concat "/sudo:root@localhost:"
+             buffer-file-name))))
+
+;;; tramp.el ends here
