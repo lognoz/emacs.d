a7e4f04e55b4027fb9179723347053e4b87c3b27
Marc-Antoine Loignon <developer@lognoz.org>
2020-10-27 22:45:19 -0400

    Lisp: Add initial winner configurations

---
+++ lisp/tools/winner.el
@@ -0,0 +1,34 @@
+;;; lisp/tools/winner.el --- winner configurations -*- lexical-binding: t; -*-
+
+;; Copyright (c) Marc-Antoine Loignon
+
+;; Author: Marc-Antoine Loignon <developer@lognoz.org>
+;; Homepage: https://github.com/lognoz/embla
+;; Keywords: winner
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Code:
+
+;;;###autoload
+(advice emacs-startup-hook winner-mode)
+
+;;;###autoload
+(bind-keys embla-mode-map
+  ("<s-left>"  . winner-undo)
+  ("<s-right>" . winner-redo))
+
+;;; winner.el ends here
